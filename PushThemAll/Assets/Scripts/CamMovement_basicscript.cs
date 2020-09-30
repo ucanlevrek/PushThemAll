d26cae30c7cf77ce26c1ad1af47ca124be92a2b2
Berkay Karslı <berkaykarsli1998@gmail.com>
2020-09-30 09:04:21 +0300

    leveldesing+fullmovement

---
+++ PushThemAll/Assets/Scripts/CamMovement_basicscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement_basicscript : MonoBehaviour
+{
+    GameObject player;
+    private Vector3 offset;
+
+    bool noplayer = false;
+  
+    
+    private void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null && noplayer == false)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            offset = transform.position - player.transform.position;
+            noplayer = true;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            gameObject.transform.position = player.transform.position + offset;
+        }
+    }
+}
---
+++ PushThemAll/Assets/Scripts/Movement.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject handle;
+    private Rigidbody rb;
+
+    public float mvspeed;
+    public float constantSpeed1, constantSpeed2, constantSpeed3;
+    private float constantSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (handle.transform.localPosition.x > 0)
+        {
+            if (handle.transform.localPosition.magnitude < 32)
+            {
+                constantSpeed = constantSpeed1;
+            }
+            else if (handle.transform.localPosition.magnitude < 64)
+            {
+                constantSpeed = constantSpeed2;
+            }
+            else
+            {
+                constantSpeed = constantSpeed3;
+            }
+
+            rb.AddForce(new Vector3(-mvspeed, 0, 0) * Time.deltaTime);
+
+            if (rb.velocity.magnitude > constantSpeed)
+            {
+                rb.velocity = constantSpeed * (rb.velocity.normalized);
+            }
+        }
+
+        if (handle.transform.localPosition.x < 0)
+        {
+            if (handle.transform.localPosition.magnitude < 32)
+            {
+                constantSpeed = constantSpeed1;
+            }
+            else if (handle.transform.localPosition.magnitude < 64)
+            {
+                constantSpeed = constantSpeed2;
+            }
+            else
+            {
+                constantSpeed = constantSpeed3;
+            }
+
+            rb.AddForce(new Vector3(mvspeed, 0, 0) * Time.deltaTime);
+
+            if (rb.velocity.magnitude > constantSpeed)
+            {
+                rb.velocity = constantSpeed * (rb.velocity.normalized);
+            }
+        }
+
+        if (handle.transform.localPosition.y > 0)
+        {
+            if (handle.transform.localPosition.magnitude < 32)
+            {
+                constantSpeed = constantSpeed1;
+            }
+            else if (handle.transform.localPosition.magnitude < 64)
+            {
+                constantSpeed = constantSpeed2;
+            }
+            else
+            {
+                constantSpeed = constantSpeed3;
+            }
+
+            rb.AddForce(new Vector3(0, 0, -mvspeed) * Time.deltaTime);
+
+            if (rb.velocity.magnitude > constantSpeed)
+            {
+                rb.velocity = constantSpeed * (rb.velocity.normalized);
+            }
+        }
+
+        if (handle.transform.localPosition.y < 0)
+        {
+            if (handle.transform.localPosition.magnitude < 32)
+            {
+                constantSpeed = constantSpeed1;
+            }
+            else if (handle.transform.localPosition.magnitude < 64)
+            {
+                constantSpeed = constantSpeed2;
+            }
+            else
+            {
+                constantSpeed = constantSpeed3;
+            }
+
+            rb.AddForce(new Vector3(0, 0, mvspeed) * Time.deltaTime);
+
+            if (rb.velocity.magnitude > constantSpeed)
+            {
+                rb.velocity = constantSpeed * (rb.velocity.normalized);
+            }
+        }
+    }
+}
